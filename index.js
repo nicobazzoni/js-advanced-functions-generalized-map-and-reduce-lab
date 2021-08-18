4e56a4a85d42628c257535a0f595f01853d2c5cd
nicobazzoni <jillianamagsila@Jillians-iMac.fios-router.home>
2021-08-17 21:10:44 -0400

    commit tree ee0c2f7aba5e384eae10f50e0e93a34b014e7503

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Add your functions here
+function map(root, call) {
+const num = []
+
+for ( let i = 0; i < root.length; i++){
+    const element = root[i]
+    num.push(call(element))
+
+}
+return num
+}
